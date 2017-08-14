a1d45ce453b47b0acd4e3f8d623f6003f85f84f6
alex.lvxin <lvxin_1986@sina.cn>
2017-08-15 01:57:54 +0800

    reconstruct

---
+++ src/main/java/com/jd/etl/consts/ETLConst.java
@@ -0,0 +1,23 @@
+package com.jd.etl.consts;
+
+import java.util.HashSet;
+import java.util.Set;
+
+/**
+ * Created by root on 17-8-14.
+ */
+public interface ETLConst {
+    public static final String ETL_THREADS = "etl.mysql2orc.threads.num";
+    public static final String ETL_THREADS_DEFAUL_VALUE = "8";
+    public static final String ETL_TABLES_PATH = "etl.mysql2orc.tables.path";
+    public static final String ETL_TAGET_PATH = "etl.mysql2orc.orc.path";
+    public static final String ETL_COALESCE = "etl.mysql2orc.orc.coalesce";
+    public static final String ETL_COALESCE_DEFAULT_VALUE = "0";
+
+    public static  Set<String> COL_TYPE_NUMBER = new HashSet<String>() {{
+        add("INT");
+        add("BIGINT");
+        add("LONG");
+    }};
+
+}
